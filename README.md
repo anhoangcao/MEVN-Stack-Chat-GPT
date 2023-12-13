60b9390aa3e84fb61e8aaa60a57867757de73433
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2023-12-13 18:15:00 +0700

    update

--- README.md
+++
@@ -1,1 +0,0 @@
-# MEVN-Stack-Chat-GPT
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <div id="chat_container">
+      <div v-for="(chat, i) in wrapper" :key="i" class="wrapper" :class="{ ai: chat.isAi }">
+        <Chat :chat="chat" :key="i" />
+      </div>
+    </div>
+    <form @submit.prevent="fetchAnswer">
+      <textarea rows="1" cols="1" placeholder="Type your question here..." v-model="question"></textarea>
+      <button type="submit"><img src="@/assets/send.svg" alt="send" /></button>
+    </form>
+  </main>
+</template>
+
+
+<script setup>
+  import { ref } from 'vue';
+  const question = ref('');
+
+  const fetchAnswer = () => {
+    console.log(question.value);
+  }
+</script>
